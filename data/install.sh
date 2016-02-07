fbd89b910f6b9ade06f4a5bb9af9c81956b1c6da
Mircea Silviu Itu <decemvre@gmail.com>
2016-02-08 00:47:25 +0200

    dynamic

--- data/install.sh
+++ data/install.sh
@@ -6,7 +6,7 @@ echo "Running install.sh for $1"
 
 echo "Cloning magento repo"
 # add version
-git clone https://decemvre:2245dfb67d208f3503081b38814bdd54e04368f7@github.com/magento/magento2.git /www
+git clone https://2245dfb67d208f3503081b38814bdd54e04368f7:x-oauth-basic@github.com/magento/magento2.git /www
 
 echo "Installing composer packages"
 cd /www && composer.phar self-update && composer.phar config -g github-oauth.github.com 2245dfb67d208f3503081b38814bdd54e04368f7 && composer.phar install --ignore-platform-reqs
--- data/list.sh
+++ data/list.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 # Should be replaces by some smart sh script which fetches the available releases from github.com/magento/magento2
-echo 2.0.1
-echo 2.0.0
+# echo 2.0.1
+# echo 2.0.0
+
+
+git ls-remote --tags https://github.com/magento/magento2.git

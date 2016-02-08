d3de87eec7caf7c8abb43cbaf0411d8a3a063d94
Mircea Silviu Itu <decemvre@gmail.com>
2016-02-08 14:12:15 +0200

    bootstrap

--- data/install.sh
+++ data/install.sh
@@ -5,8 +5,16 @@ echo "Running install.sh for $1"
 # TODO: Download selected version via  (from docker-sho-manager) from githup.com/magento/magento2 and run installer
 
 echo "Cloning magento repo"
-# add version
 git clone https://2245dfb67d208f3503081b38814bdd54e04368f7:x-oauth-basic@github.com/magento/magento2.git /www
 
 echo "Installing composer packages"
 cd /www && composer.phar self-update && composer.phar config -g github-oauth.github.com 2245dfb67d208f3503081b38814bdd54e04368f7 && composer.phar install --ignore-platform-reqs
+
+echo "Setting filesystem permissions"
+chown -R www-data /www
+chgrp -R www-data /www
+
+echo "Checking out $1"
+checkOutCommand="git checkout $1"
+eval $checkOutCommand
+echo "Installation completed"

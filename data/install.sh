98a393eff63294a17be9fb3dbbb5e01b5736b4f6
Mircea Silviu Itu <decemvre@gmail.com>
2016-02-22 11:14:59 +0200

    install mod

--- data/install.sh
+++ data/install.sh
@@ -5,10 +5,10 @@ echo "Running install.sh for $1"
 # TODO: Download selected version via  (from docker-sho-manager) from githup.com/magento/magento2 and run installer
 
 echo "Cloning magento repo"
-git clone --branch $1 --depth 1 https://2245dfb67d208f3503081b38814bdd54e04368f7:x-oauth-basic@github.com/magento/magento2.git /www
+git clone --branch $1 --depth 1 https://$2:x-oauth-basic@github.com/magento/magento2.git /www
 
 echo "Installing composer packages"
-cd /www && composer.phar self-update && composer.phar config -g github-oauth.github.com 2245dfb67d208f3503081b38814bdd54e04368f7 && composer.phar install --ignore-platform-reqs
+cd /www && composer.phar self-update && composer.phar config --global github-oauth.github.com $2 && composer.phar update --ignore-platform-reqs
 
 echo "Setting filesystem permissions"
 chown -R www-data /www

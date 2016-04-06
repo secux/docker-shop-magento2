baeb074702857eacfd41f4be8dc419b66ec8ba3e
Mircea Silviu Itu <decemvre@gmail.com>
2016-04-06 09:04:23 +0300

    fixed Magento2 frontend

--- data/install.sh
+++ data/install.sh
@@ -10,6 +10,27 @@ cd  /www \
     && composer.phar self-update \
     && composer.phar install --ignore-platform-reqs
 
+echo "Automatically installing your Magento 2 shop with the following params: \n 
+--admin-firstname=Ad
+--admin-lastname=Minator
+--admin-email=ad.minator@adminat.ro
+--admin-user=Adminator
+--admin-password=secu117781
+--base-url=http://localhost/
+--backend-frontname=admin
+--db-host=database
+--db-name=secu
+--db-user=secu
+--db-password=secu"
+
+php bin/magento setup:install --admin-firstname=Ad --admin-lastname=Minator --admin-email=ad.minator@adminat.ro --admin-user=Adminator --admin-password=secu117781 --base-url=http://localhost:6090/ --backend-frontname=admin --db-host=database --db-name=secu --db-user=secu --db-password=secu
+
+# php -r "phpinfo();"
+# php -m
+
+echo "Installing Magento 2 static files"
+php bin/magento setup:static-content:deploy
+
 echo "Setting filesystem permissions"
 chown -R www-data /www
 chgrp -R www-data /www

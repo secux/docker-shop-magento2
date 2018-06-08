0fe1c03b8f68b97926b6d909ad399932a4b9f281
Rico Simlinger <r.simlinger@secupay.ag>
2018-06-08 14:14:33 +0200

    Update install.sh

--- data/install.sh
+++ data/install.sh
@@ -5,6 +5,13 @@ echo "Running install.sh for $1 using $2 as server name"
 echo "Cloning magento repo"
 git clone --branch $1 --depth 1 https://github.com/magento/magento2.git /www
 
+echo "Setting filesystem permissions"
+chmod -R 777 /www/generated
+chmod -R 777 /www/pub/static
+chmod -R 777 /www/pub/media
+chmod -R 777 /www/var
+chmod -R 777 /www/app/etc
+
 echo "Installing composer packages"
 cd  /www \
     && composer.phar self-update \
@@ -36,11 +43,5 @@ php bin/magento setup:static-content:deploy
 echo "Setting filesystem permissions"
 chown -R www-data /www
 chgrp -R www-data /www
-
-chmod -R 777 /www/generated
-chmod -R 777 /www/pub/static
-chmod -R 777 /www/pub/media
-chmod -R 777 /www/var
-chmod -R 777 /www/app/etc
 
 echo "Installation completed"
